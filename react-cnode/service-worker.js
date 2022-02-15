7d52323129e6a67b012e2cb4f9440acce2cc4e50
guoqirong <2834964850@qq.com>
2022-02-15 21:42:04 +0800

    react 项目修改

--- react-cnode/service-worker.js
+++ react-cnode/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cefcc5dd8354496afd57d439c5e565be.js"
+  "./react-cnode/precache-manifest.5da63a11ed94a403ea993078cf53ebd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./react-cnode/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
